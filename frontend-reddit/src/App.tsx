68293d788ea326d9e0340104f874a111e56785ff
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-03-02 02:19:45 +0600

    frontend: add container OnePost

--- frontend-reddit/src/App.tsx
+++ frontend-reddit/src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
-import { Container, CssBaseline } from '@mui/material';
+import { useAppSelector } from './app/hooks';
+import { selectUser } from './features/users/usersSlice';
 import AppToolbar from './components/UI/AppToolbar/AppToolbar';
 import Register from './features/users/Register';
 import Login from './features/users/Login';
-import { useAppSelector } from './app/hooks';
-import { selectUser } from './features/users/usersSlice';
 import NewPost from './features/posts/NewPost';
 import NotFound from './components/NotFound';
 import Post from './features/posts/Post';
+import OnePost from './features/posts/OnePost';
+import { Container, CssBaseline } from '@mui/material';
 
 const App = () => {
   const user = useAppSelector(selectUser);
@@ -25,6 +26,7 @@ const App = () => {
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             {user ? <Route path="/add-new-post" element={<NewPost />} /> : ''}
+            <Route path="/post/:id" element={<OnePost />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </Container>
---
+++ frontend-reddit/src/features/posts/OnePost.tsx
@@ -0,0 +1,70 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import { fetchOnePost } from './postThunks';
+import { selectOnePost, selectOnePostFetchLoading } from './postSlice';
+import {
+  Box,
+  CardMedia,
+  CircularProgress,
+  Grid,
+  Typography,
+} from '@mui/material';
+import LocalizedFormat from 'dayjs/plugin/localizedFormat';
+import dayjs from 'dayjs';
+import imageMessage from '../../assets/images/message.png';
+import { apiURL } from '../../constants';
+
+dayjs.extend(LocalizedFormat);
+
+const OnePost = () => {
+  const { id } = useParams() as { id: string };
+  const dispatch = useAppDispatch();
+  const onePost = useAppSelector(selectOnePost);
+  const onePostLoading = useAppSelector(selectOnePostFetchLoading);
+
+  let cardImage = imageMessage;
+
+  if (onePost?.image) {
+    cardImage = apiURL + '/' + onePost.image;
+  }
+
+  useEffect(() => {
+    dispatch(fetchOnePost(id));
+  }, [dispatch, id]);
+
+  return (
+    <>
+      {onePostLoading && onePost ? (
+        <CircularProgress />
+      ) : (
+        onePost && (
+          <Grid container>
+            <Grid item xs={4} sx={{ maxWidth: '350px' }}>
+              <Box sx={{ maxWidth: '350px' }}>
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={cardImage}
+                  alt={onePost.title}
+                />
+              </Box>
+            </Grid>
+            <Grid item xs={8}>
+              <Typography variant="h4">
+                Author: {onePost.author.username}
+              </Typography>
+              <Typography variant="h6">
+                {dayjs(onePost.createdAt).format('lll')}
+              </Typography>
+              <Typography variant="h4">{onePost.title}</Typography>
+              <Typography component="p">{onePost.description}</Typography>
+            </Grid>
+          </Grid>
+        )
+      )}
+    </>
+  );
+};
+
+export default OnePost;
